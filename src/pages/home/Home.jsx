ccb6fafc877c6e1272b303a69735562a28aedaa7
yuvrajsinghnegi012 <144053547+yuvrajsinghnegi012@users.noreply.github.com>
2023-09-17 14:01:30 +0530

    Update Home.jsx
    Just updates the imported style.scss naming

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 
-import './style.scss';
+import './Style.scss';
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
 import Popular from './popular/Popular';
