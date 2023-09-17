8e1c7d0e58065dad59390c3ea31314fb7feb43a2
yuvrajsinghnegi012 <144053547+yuvrajsinghnegi012@users.noreply.github.com>
2023-09-17 14:03:21 +0530

    Update Home.jsx
    stylesheet name changed back to previous one

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 
-import './Style.scss';
+import './style.scss';
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
 import Popular from './popular/Popular';
